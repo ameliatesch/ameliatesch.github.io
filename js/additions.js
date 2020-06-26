044508881d3a4ced669320900a2030e98aef5345
Nathan Smith <mcaeolusch@gmail.com>
2020-06-26 12:52:39 -0400

    More header fixes for small devices (ex iPhone X), fix large navbar for small devices in HEB modal

--- js/additions.js
+++ js/additions.js
@@ -15,11 +15,25 @@ $(document).ready(function() {
         });
     });
 
+    fixHeaderMargin();
+
+    
+});
+
+$(window).resize(function() {
+    fixHeaderMargin();
+
+});
+
+function fixHeaderMargin() {
     if($(window).width() <= 500) {
 
-        var indent = -0.6 - (500 - $(window).width())*0.001
+
+        var indent = -0.6 - (500 - $(window).width())*0.0055
         console.log(indent);
 
         $('.hebheaderfix').css({"text-indent": indent + 'em', "margin-left":'0em'});
+    } else {
+        $('.hebheaderfix').css({"text-indent":'0em', "margin-left":'0em'});
     }
-});
+}
